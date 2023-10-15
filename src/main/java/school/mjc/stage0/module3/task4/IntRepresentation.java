ebb0b5e72d63ce3aa444057d999ff20a14b7fed5
Нодир Асомов <nasomov93@gmail.com>
2023-10-15 10:14:53 +0500

    hi

--- src/main/java/school/mjc/stage0/module3/task4/IntRepresentation.java
+++ src/main/java/school/mjc/stage0/module3/task4/IntRepresentation.java
@@ -2,5 +2,8 @@ package school.mjc.stage0.module3.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
+        char one ='c', two ='h', three ='a', four = 'r';
+
+        System.out.println(one + two +three +four);
     }
 }

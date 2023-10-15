9df88cf93b72e6679e6a65710f08935dee87e464
Нодир Асомов <nasomov93@gmail.com>
2023-10-15 10:37:11 +0500

    hi

--- src/main/java/school/mjc/stage0/module3/task4/IntRepresentation.java
+++ src/main/java/school/mjc/stage0/module3/task4/IntRepresentation.java
@@ -2,8 +2,16 @@ package school.mjc.stage0.module3.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
-        char one ='c', two ='h', three ='a', four = 'r';
 
-        System.out.println(one + two +three +four);
+
+        char ch = 'c';
+        char sh ='h';
+        char th='a';
+        char ph='r';
+
+        // Printing the character value
+        System.out.println(ch+"\n"+sh+"\n"+th+"\n"+ph);
+
+
     }
 }
--- src/main/java/school/mjc/stage0/module3/task4/SettingTypes.java
+++ src/main/java/school/mjc/stage0/module3/task4/SettingTypes.java
@@ -3,11 +3,11 @@ package school.mjc.stage0.module3.task4;
 public class SettingTypes {
     public static void main(String[] args) {
         long first = 10_000_000_000L;
-       char second = 'a';
+       char  second = 'a';
         boolean third = 5 > 9;
         double forth = 12.90434323324342;
         float fifth = 12.3F;
-        int sixth = 1;
+        byte sixth = 1;
         int seventh = 129;
     }
 }
